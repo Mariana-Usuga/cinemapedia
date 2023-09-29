e4ab430f576972262c2398812890752ca9eb77a7
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-09-29 14:54:46 -0500

    routers and themes

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const name = 'home-screen';
+
+  const HomeScreen({super.key});
+  Widget build(BuildContext context) {
+    return Scaffold(body: Placeholder());
+  }
+}
