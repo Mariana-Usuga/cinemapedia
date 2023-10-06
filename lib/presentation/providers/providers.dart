34982f047150ccccd53951e5051df9ae67fac584
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-10-06 08:52:24 -0500

    import actorByMovieProviders

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,3 +1,5 @@
+export 'package:cinemapedia/presentation/providers/actors/actors_by_movie_providers.dart';
+
 export 'movies/movies_providers.dart';
 export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slideshow_provider.dart';
