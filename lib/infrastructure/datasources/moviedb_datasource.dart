6302826fd1be840be9bc6b2c2166e331448ea553
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-10-07 09:20:32 -0500

    finish search movies

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -76,4 +76,14 @@ class MoviedbDatasource extends MoviesDatasource {
 
     return movieDetail;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    if (query.isEmpty) return [];
+
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+
+    return _jsonToMovies(response.data);
+  }
 }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,10 +1,19 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
@@ -25,7 +34,34 @@ class CustomAppbar extends StatelessWidget {
               const SizedBox(width: 10),
               Text('Cinemapedia', style: titleStyle),
               const Spacer(),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+              IconButton(
+                  onPressed: () async {
+                    //final movieRepository = ref.read(movieRepositoryProvider);
+                    final searchedMovies = ref.read(searchedMoviesProvider);
+                    final searchQuery = ref.read(searchQueryProvider);
+                    //delagate:se encarga de trabajar la busqueda
+                    showSearch<Movie?>(
+                            query: searchQuery,
+                            context: context,
+                            delegate: SearchMovieDelegate(
+                                initialMovies: searchedMovies,
+                                searchMovies: ref
+                                    .read(searchedMoviesProvider.notifier)
+                                    .searchMoviesByQuery
+                                /*(query) {
+                              ref
+                                  .read(searchQueryProvider.notifier)
+                                  .update((state) => query);
+                              return searchedMoviesProvider.searchMovies(query);
+                            }*/
+                                )) // dar la referencia
+                        .then((movie) {
+                      if (movie == null) return;
+                      context.push('/movie/${movie.id}');
+                    });
+                    //print(movie?.title);
+                  },
+                  icon: const Icon(Icons.search))
             ],
           ),
         ),
